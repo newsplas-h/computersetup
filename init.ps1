ae6400390092c5451bcf9a867de1b70848a3f2c8
Ethan <50597750+newsplas-h@users.noreply.github.com>
2025-07-30 15:32:28 -0400

    Create init.ps1

---
+++ init.ps1
@@ -0,0 +1,13 @@
+$TempPath = "$env:windir\Temp\Provisioning"
+
+# Create temp directory
+New-Item -Path $TempPath -ItemType Directory -Force | Out-Null
+
+Invoke-WebRequest -Uri "https://github.com/newsplas-h/computersetup/raw/refs/heads/main/default.ppkg" -OutFile "$TempPath\setup.ppkg" -UseBasicParsing
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/newsplas-h/computersetup/refs/heads/main/autorun.ps1" -OutFile "$TempPath\install.ps1" -UseBasicParsing
+
+# Apply ppkg
+Install-ProvisioningPackage -PackagePath "$TempPath\setup.ppkg" -QuietInstall -ForceInstall
+
+# Run cleanup
+Remove-Item -Path $TempPath -Recurse -Force
